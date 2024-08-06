02b5ef302fc8de81722465fcd858f2212dbb07da
Joaquin Villalba <joaquinvillalba8@gmail.com>
2024-08-06 19:25:47 -0300

    delete old css

--- src/Components/OLists.jsx
+++ src/Components/OLists.jsx
@@ -61,27 +61,23 @@ const OLists = ({ title, lista }) => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-lg-12">
-          <div className="page-content">
-            <div className="gaming-library">
-              <div className="row">
-                <div className="heading-section">
-                  <h4 className="cat-title" align="center">
-                    <em>{title}</em>
-                  </h4>
-                </div>
-
-                {lista.map((director) => (
-                  <NewCard
-                    key={director[0]}
-                    src={localimg(director[0]) || handleImg(director[0])}
-                    title={director[0]}
-                    year={director[1]}
-                    list={moviesList(director[0])}
-                  ></NewCard>
-                ))}
-              </div>
+        <div className="page-content">
+          <div className="row">
+            <div className="heading-section">
+              <h4 className="cat-title" align="center">
+                <em>{title}</em>
+              </h4>
             </div>
+
+            {lista.map((director) => (
+              <NewCard
+                key={director[0]}
+                src={localimg(director[0]) || handleImg(director[0])}
+                title={director[0]}
+                year={director[1]}
+                list={moviesList(director[0])}
+              ></NewCard>
+            ))}
           </div>
         </div>
       </div>
