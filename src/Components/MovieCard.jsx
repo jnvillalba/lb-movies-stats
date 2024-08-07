f3459699e9f333f0b17389708daa695af1827d67
Joaquin Villalba <joaquinvillalba8@gmail.com>
2024-08-06 21:20:02 -0300

    fix eslint

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -2,8 +2,10 @@ import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import cast from "../Lists/cast";
 import "./MovieCard.css";
+
 const MovieCard = ({ movie }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [poster, setPoster] = useState();
 
   const toggleOpen = () => {
     setIsOpen(!isOpen);
@@ -14,34 +16,33 @@ const MovieCard = ({ movie }) => {
     let img = localimg ? localimg.img : "";
     return img;
   };
-  const [poster, setPoster] = useState();
 
   useEffect(() => {
-    moviePoster(movie.name);
-  }, [movie.name]);
+    const moviePoster = (name) => {
+      if (movie.img) {
+        setPoster(movie.img);
+      } else {
+        fetch(
+          `https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=${name}`
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            if (json.results.length > 0) {
+              setPoster(
+                `http://image.tmdb.org/t/p/w500/${json.results[0].poster_path}`
+              );
+            } else {
+              setPoster("Not Found");
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
+    };
 
-  const moviePoster = (name) => {
-    if (movie.img) {
-      setPoster(movie.img);
-    } else {
-      fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=${name}`
-      )
-        .then((response) => response.json())
-        .then((json) => {
-          if (json.results.length > 0) {
-            setPoster(
-              `http://image.tmdb.org/t/p/w500/${json.results[0].poster_path}`
-            );
-          } else {
-            setPoster("Not Found");
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  };
+    moviePoster(movie.name);
+  }, [movie.name, movie.img]);
 
   return (
     <motion.div
@@ -80,9 +81,9 @@ const MovieCard = ({ movie }) => {
           <p className="mt-2">
             <em>Cast:</em>
           </p>
-          {movie.actors.slice(0, 7).map((writer, i) => (
+          {movie.actors.slice(0, 7).map((actor, i) => (
             <p className="px-2" key={i}>
-              {writer}
+              {actor}
             </p>
           ))}
         </motion.div>
