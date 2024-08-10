e100216b2ac91ea4d2ee63c625649b6f4fc30304
Joaquin Villalba <joaquinvillalba8@gmail.com>
2024-08-10 19:36:30 -0300

    CHG: Better detail page

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeadingSection from "./HeadingSection";
 import MovieCard from "./MovieCard/MovieCard.jsx";
 
-const Details = ({ title, lista }) => {
+const Details = ({ title, lista, count }) => {
+  const titleWithCount = `${title} (${count})`;
+
   return (
     <div className="container">
       <div className="page-content">
         <div className="row justify-content-center">
-          <HeadingSection title={title} />
+          <HeadingSection title={count ? titleWithCount : title} />
 
           {lista.map((movie, i) => (
             <MovieCard key={i} movie={movie} />
--- src/Components/YearsMovies.jsx
+++ src/Components/YearsMovies.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useParams } from "react-router-dom";
 import Vol8 from "../Lists/Vol8";
 import Details from "./Details";
+
 const Years = () => {
   const { id } = useParams();
-  const moviesID = Vol8.filter((objeto) => objeto.year === id);
-  return <Details title={`${id}`} lista={moviesID} />;
+  const isNumeric = /^[0-9]+$/.test(id);
+
+  const moviesID = useMemo(() => {
+    if (isNumeric) {
+      return Vol8.filter((objeto) => objeto.year === id);
+    }
+
+    const allMovies = (name) => {
+      const filteredMovies = Vol8.reduce((acc, movie) => {
+        if (
+          movie.actors.includes(name) ||
+          movie.directors.includes(name) ||
+          movie.writers.includes(name)
+        ) {
+          acc.push(movie);
+        }
+        return acc;
+      }, []);
+
+      return [...new Set(filteredMovies)];
+    };
+
+    return allMovies(id);
+  }, [id, isNumeric]);
+
+  return <Details title={`${id}`} lista={moviesID} count={moviesID.length} />;
 };
 
 export default Years;
