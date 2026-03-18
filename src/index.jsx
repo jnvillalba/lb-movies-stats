9989d62a1c2ab672b72e0c7da02ae5f83db4b509
Joaquin Villalba <joaquinvillalba8@gmail.com>
2026-03-18 19:43:49 -0300

    refactor: migrate from Create React App to Vite

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./App.css";
+import "./index.css";
+import "./Style/templatemo-cyborg-gaming.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
